e2e7c2cccc82a8e25bcdb197e332009e383de21a
Kire Popovski <kpopovski02@gmail.com>
2024-04-02 23:51:47 +0200

    Add starter code

---
+++ SILab1_2.java
@@ -0,0 +1,16 @@
+package Desktop.SI_2024_lab1_211222;
+
+import java.util.List;
+
+public class SILab1_2 {
+
+    private static String makeBigStringFrom (List<String> list, int minLength) {
+        StringBuilder sb = new StringBuilder();
+        for (String el : list) {
+            if (el.length() < minLength) {
+                sb.append(el);
+            }
+        }
+        return sb.toString();
+    }
+}
